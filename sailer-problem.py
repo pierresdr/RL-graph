c85c8b466546b640d2340732cde19eaeb8895125
robustepierre <robustepierre@users.noreply.github.com>
2019-01-07 17:02:58 +0100

    Update sailer-problem.py

--- sailer-problem.py
+++ sailer-problem.py
@@ -162,7 +162,7 @@ class state:
     
 
 
-class MDP:
+class MDP_IRL:
     #   Class which contains all the states, and their return and probabilities
     def __init__(self,action_by_state,gamma):
         self.nb_states = len(action_by_state)
@@ -183,6 +183,126 @@ class MDP:
         return result
     
         
+#%%
+        
+
+class state_action:
+    def __init__(self,r,P):
+        self.r = r
+        self.P = P
+
+    def bellman_action(self,V,gamma):
+        #   For QUESTION 2
+        #   Computes Bellman Operator for a given state and action
+        return(self.r+gamma*np.dot(self.P,V))
+
+
+
+class state:
+    #   Class which contains the returns and probabilities for a given state
+    def __init__(self,r,P):
+        self.nb_actions = len(r)
+        self.actions = list()
+        for i in range(self.nb_actions):
+            self.actions.append(state_action(r[i],P[i,:]))
+        self.policy = np.ones(self.nb_actions)/self.nb_actions
+    
+    def bellman_opt(self,V,gamma):
+        #   For QUESTION 2
+        #   Computes the optimal Bellman operator over the possible actions for a given state
+        temp = 0
+        for j in range(self.nb_actions):
+            temp = max(temp,self.actions[j].bellman_action(V,gamma))
+        return (temp)
+
+    def make_trajectory(self):
+        #   For QUESTION 3
+        """temp1 = np.cumsum(self.policy)
+        random1 = np.random.rand()
+        chosen_action = np.where(temp1>=random1)[0][0]"""
+        chosen_action = np.where(self.policy==1)[0][0]
+        temp2 = np.cumsum(self.actions[chosen_action].P)
+        random2 = np.random.rand()
+        new_state = np.where(temp2>=random2)[0][0]
+        return self.actions[chosen_action].r, new_state
+    
+    def set_policy(self,init_policy):
+        self.policy = init_policy
+    
+    def selectAction(self):
+        actions = np.cumsum(self.policy)
+        return  np.where(actions>=np.random.rand())[0][0]
+    
+    def getNewState(self,chosen_action):
+        new_states = np.cumsum(self.actions[chosen_action].P)
+        return np.where(new_states>=np.random.rand())[0][0]
+        
+    
+
+
+class MDP:
+    #   Class which contains all the states, and their return and probabilities
+    def __init__(self,r,P,gamma):
+        self.nb_states = len(r)
+        self.states=list()
+        self.V = np.zeros(self.nb_states)
+        self.gamma = gamma
+        
+        #build the states contained in MDP
+        self.action_by_state=np.cumsum(action_by_state)
+        sub = range(self.action_by_state[0])
+        self.states.append(state(r[sub],P[sub,:]))
+        for i in range(self.nb_states-1 ):
+            sub = range(self.action_by_state[i],self.action_by_state[i+1])
+            self.states.append(state(r[sub],P[sub,:]))
+    
+    def trajectory(self,j,T):
+        #   For QUESTION 3
+        result, new_state = self.states[j].make_trajectory()
+        for i in range(T):
+            temp, new_state = self.states[new_state].make_trajectory()
+            result += gamma**(i+1)*temp
+        return result
+    
+    def createDR(self,n):
+        states = np.random.randint(0,nb_states,n)
+        DR = np.zeros(3,n)
+        for i in range(n):
+            DR[0,i] = i
+            DR[1,i] = self.states[i].selectAction()
+            DR[2,i] = self.states[i].getNewState(DR[1,i])
+        return DR
+            
+    
+    def get_policy(self):
+        #   For QUESTION 2 and QUESTION 3
+        policy = np.zeros(self.action_by_state[len(self.action_by_state)-1])
+        sub = range(self.action_by_state[0])
+        policy[sub] = self.states[0].policy
+        for i in range(self.nb_states-1 ):
+            sub = range(self.action_by_state[i],self.action_by_state[i+1])
+            policy[sub] = self.states[i+1].policy
+        return policy
+
+
+gamma = 0.95
+
+
+
+r = np.array([0,0,5/100,0,0,0,0,1,9/10]) #the returns of each (x,a), in the right order
+P = np.array([[0.55,0.45,0], #the probabilities p(y|x,a) for each (x,a) in the right order
+             [0.3,0.7,0],
+             [1,0,0],
+             [1,0,0],
+             [0,0.4,0.6],
+             [0,1,0],
+             [0,1,0],
+             [0,0.6,0.4],
+             [0,0,1]])
+
+MDP_opt = MDP(r,P,gamma)
+    
+MDP_IRL = MDP_IRL(action_by_state,gamma)   
     
 
 def CSI(DC,DR,action_by_state,gamma):
@@ -195,6 +315,6 @@ def CSI(DC,DR,action_by_state,gamma):
     r = MDP(action_by_state,gamma)
     
     for i in range(np.shape(DR)[1]):
-        r.state[DR[0,i]] = classifier.score(DR[0,i],DR[1,i]) - gamma * classifier.score(DR[0,i],classifier)
+        r.state[DR[0,i]] = classifier.score(DR[0,i],DR[1,i]) - gamma * classifier.score(DR[2,i],classifier)
         
     return r
